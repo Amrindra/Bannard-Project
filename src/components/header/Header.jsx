fe9119d55935ebe9f703a5880babb5662be85e94
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-09 22:56:03 -0400

    Created Header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
 
 const Header = () => {
   return (
@@ -9,10 +8,11 @@ const Header = () => {
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="justify-content-end nav">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
