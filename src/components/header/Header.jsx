0b368cea2f5e100c6aa4b166eea34e56ad868467
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-12 17:30:49 -0400

    created homepage dir and component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,16 +3,30 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="md"
+      collapseOnSelect
+      className="p-4"
+    >
       <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="justify-content-end nav">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+        <Navbar.Collapse id="responsive-navbar-nav mr-auto">
+          <Nav className="justify-content-end nav ">
+            <Nav.Link href="#home" className="nav_item ">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#features" className="nav_item">
+              Features
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="nav_item">
+              Products
+            </Nav.Link>
+            <Nav.Link href="#contact" className="nav_item">
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="homepage_container">This is home papage</div>
+    </div>
+  );
+};
+
+export default Homepage;
