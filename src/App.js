ff6a3b2914742a594e74a3ac4d2ae29bb97e64dc
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-12 17:35:45 -0400

    Installed React Router Dom and setup routes in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
+import Homepage from "./pages/homepage/Homepage";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
       <Header />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
