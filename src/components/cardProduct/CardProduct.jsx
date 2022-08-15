111d6796dba3f972b925455ee8a5b8ef0f345a6b
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-15 17:53:11 -0400

    Coded Card Component

---
+++ src/components/cardProduct/CardProduct.jsx
@@ -0,0 +1,28 @@
+import "./CardProduct.css";
+
+function CardProduct() {
+  return (
+    <div className="card_product">
+      <div className="card_product_warpper">
+        <div className="card_top">
+          <img
+            src="https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png"
+            alt="card_img"
+          />
+        </div>
+        <div className="card_body">
+          <h3 className="card_title">Card Title</h3>
+          <p className="card_desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+            explicabo, eaque, veritatis et delectus, quibusdam similique iste
+            dolorum iure blanditiis ipsam. Ratione impedit illo rem ab
+            perspiciatis voluptatum perferendis veniam.
+          </p>
+          <button className="card_button">Check me out</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardProduct;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import { Carousel, Container } from "react-bootstrap";
 import Banner from "../../components/banner/Banner";
+import CardProduct from "../../components/cardProduct/CardProduct";
 import CarouselSlider from "../../components/carouselSlider/CarouselSlider";
 
 import "./Homepage.css";
@@ -7,8 +8,21 @@ import "./Homepage.css";
 const Homepage = () => {
   return (
     <div className="homepage">
-      {/* <Banner /> */}
+      <Banner />
       <CarouselSlider />
+
+      <section className="card_sections">
+        <Container>
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+        </Container>
+      </section>
     </div>
   );
 };
