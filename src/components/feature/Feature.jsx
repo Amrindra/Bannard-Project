3778bda39ca41455e76e9f015cf5b72813701ffa
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-16 18:52:39 -0400

    Designed CSS for Feature component and added root to global style

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -11,21 +11,25 @@ const Feature = () => {
           />
         </div>
         <div className="feature_top_right">
-          <h3 className="feature_title">Lorem ipsum dolor sit amet.</h3>
-          <p className="feature_desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-            fugiat et facere officiis, voluptatem aliquam.
-          </p>
+          <div className="feature_body_wrapper">
+            <h3 className="feature_title">Lorem ipsum dolor sit.</h3>
+            <p className="feature_desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              fugiat et facere officiis, voluptatem aliquam.
+            </p>
+          </div>
         </div>
       </div>
 
       <div className="feature_bottom">
         <div className="feature_bottom_right">
-          <h3 className="feature_title">Lorem ipsum dolor sit amet.</h3>
-          <p className="feature_desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-            fugiat et facere officiis, voluptatem aliquam.
-          </p>
+          <div className="feature_body_wrapper">
+            <h3 className="feature_title">Lorem ipsum dolor sit.</h3>
+            <p className="feature_desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              fugiat et facere officiis, voluptatem aliquam.
+            </p>
+          </div>
         </div>
         <div className="feature_bottom_left">
           <img
