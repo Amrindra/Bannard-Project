9255114e7cf1e941e086418c2c18da4bc3e99342
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-17 17:17:18 -0400

    added HTML structures to the contact page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
+import Contact from "./components/contact/Contact";
 import Homepage from "./pages/homepage/Homepage";
 import ProductPage from "./pages/product/ProductPage";
 import "./styles.css";
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/products" element={<ProductPage />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,24 @@
 import "./Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <div className="contact_wrapper">
+        <h1 className="contact_title">Contact Us</h1>
+        <form className="contact">
+          <div className="input_name_section">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="input_email_message_section">
+            <input type="email" placeholder="your email" />
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <button className="input_button">Send Message</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
