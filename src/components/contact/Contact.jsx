ded8abdad899a539ca9339b351cbf06b4955f05c
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-17 17:42:24 -0400

    Implemented Contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,23 +1,32 @@
+import { Container } from "react-bootstrap";
 import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <div className="contact_wrapper">
-        <h1 className="contact_title">Contact Us</h1>
-        <form className="contact">
-          <div className="input_name_section">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-          </div>
-          <div className="input_email_message_section">
-            <input type="email" placeholder="your email" />
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            <button className="input_button">Send Message</button>
-          </div>
-        </form>
+    <Container>
+      <div className="contact">
+        <div className="contact_wrapper">
+          <h1 className="contact_title">Contact Us</h1>
+          <form className="contact_form">
+            <div className="input_name_section">
+              <input type="text" placeholder="First Name" required />
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <div className="input_email_message_section">
+              <input type="email" placeholder="your email" />
+              <textarea
+                name=""
+                id=""
+                cols="20"
+                rows="10"
+                placeholder="Message"
+              ></textarea>
+              <button className="input_button">Send Message</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
