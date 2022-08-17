61eb5a74f450cdd1e45d90758833c8a76702a010
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-17 11:11:51 -0400

    import productData and passed them to each of card component

--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,32 +1,21 @@
 import { Col, Container, Row } from "react-bootstrap";
 import CardProduct from "../../components/cardProduct/CardProduct";
+import { cardProducts } from "../../utils/data";
 
 const ProductPage = () => {
   return (
     <div className="product_page">
       <Container>
         <Row>
-          <Col sm={12} md={6} lg={4} xl={4} className="pt-5">
-            <CardProduct />
-          </Col>
-          <Col sm={12} md={6} lg={4} xl={4} className="pt-5">
-            <CardProduct />
-          </Col>
-          <Col sm={12} md={6} lg={4} xl={4}>
-            <CardProduct />
-          </Col>
-          <Col sm={12} md={6} lg={4} xl={4}>
-            <CardProduct />
-          </Col>
-          <Col sm={12} md={6} lg={4} xl={4}>
-            <CardProduct />
-          </Col>
-          <Col sm={12} md={6} lg={4} xl={4}>
-            <CardProduct />
-          </Col>
-          <Col sm={12} md={6} lg={4} xl={4}>
-            <CardProduct />
-          </Col>
+          {cardProducts.map((item) => (
+            <Col sm={12} md={6} lg={4} xl={4} className="pt-5">
+              <CardProduct
+                cardImg={item.cardImg}
+                cardTitle={item.cardTitle}
+                cardDesc={item.cardDesc}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,1 +1,74 @@
-export const cardProducts = [];
+export const cardProducts = [
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  },
+  {
+    id: 1,
+    cardImg:
+      "https://user-images.githubusercontent.com/70451928/184723528-a522a10a-3a3a-46bd-98e6-024f658926e7.png",
+    cardTitle: "Card Title",
+    cardDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optioexplicabo, eaque, veritatis et delectus, quibusdam similique istedolorum iure blanditiis ipsam. Ratione impedit illo rem abperspiciatis voluptatum perferendis veniam.",
+    cardButton: ""
+  }
+];
