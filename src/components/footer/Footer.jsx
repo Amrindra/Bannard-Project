c73d357a8cf84adf6eb611c94796f5e1173c26ee
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-23 19:24:25 -0400

    Structured the Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,49 @@
+import { Container } from "react-bootstrap";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <ul className="footer">
-        <li className="footer_item">Test</li>
-        <li className="footer_item">Test</li>
-        <li className="footer_item">Test</li>
-        <li className="footer_item">Test</li>
-        <li className="footer_item">Test</li>
-        <li className="footer_item">Test</li>
-      </ul>
+      <Container>
+        <div className="footer_wrapper">
+          <div className="footer_logo">
+            <h1>T-Rid</h1>
+          </div>
+
+          <div className="footer_left">
+            <ul className="footer_list">
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+            </ul>
+          </div>
+
+          <div className="footer_center">
+            <ul className="footer_list">
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+            </ul>
+          </div>
+
+          <div className="footer_right">
+            <ul className="footer_list">
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+              <li className="footer_item">Test</li>
+            </ul>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
